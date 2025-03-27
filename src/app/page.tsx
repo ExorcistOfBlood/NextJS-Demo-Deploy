9dfff3f01fac0f84e6e53401beca2e7c231d5d9d
ExorcistOfBlood <jamnanazaza1@gmail.com>
2025-03-27 18:59:36 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Header } from "@/sections/Header";
+import { HeroSection } from "@/sections/Hero";
+
+
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+    </div>
+  );
+}
