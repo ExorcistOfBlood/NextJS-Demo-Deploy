9eed7eae36548a933f58030ebe6a93a1bda558a4
ExorcistOfBlood <jamnanazaza1@gmail.com>
2025-04-17 21:53:58 +0700

    modified:   src/app/page.tsx 	modified:   src/sections/Projects.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
-
+import { ProjectsSection } from "@/sections/Projects";
 
 
 
@@ -10,6 +10,7 @@ export default function Home() {
     <div>
       <Header />
       <HeroSection />
+      <ProjectsSection />
     </div>
   );
 }
